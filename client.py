c67b05d5bdcb4ae3d2d02f5062eb8cedc54ba086
JoshuaJWeldon <joshuajweldon@gmail.com>
2017-03-15 14:55:31 -1000

    Added comments

--- client.py
+++ client.py
@@ -1,21 +1,50 @@
-# Echo server program
+###########################################
+#
+# client.py
+#
+# This application allows you to send
+# and recieve a single message from a
+# listening server.py
+#
+# usage: client.py <server IP> <port>
+#
+#   - server IP: The IP address of the
+#                host running the server.py
+#                application
+#
+#   - port:      The port number the host is
+#                listening on
+#
+#   NOTE: This application does not check for
+#         improper input.
+#
+############################################
+
 import sys
 import socket
 
+# Print out usage statement, if the user has provided commandline arguments
 if len(sys.argv) != 3:
     print 'usage:', sys.argv[0], '<server IP> <port>'
     sys.exit(0)
 
+
+# Gather the HOST IP and PORT number from commandline arguments
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
+
+# Create a socket using IPv4, and connect to the host
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+# Get message from the user and send it to the host
 data = raw_input('what to send: ')
 s.sendall(data)
+
+# Recv and print return message from host
 data = s.recv(1024)
 print 'return msg:  ', data
 
+# Close the socket and connection
 s.close()
-
 
--- server.py
+++ server.py
@@ -1,26 +1,48 @@
-# Echo server program
+###########################################
+#
+# server.py
+#
+# This application listens and responds to
+# a single messagea from a client
+#
+# usage: server.py <port>
+#
+#   - port: The port number the application
+#           should listen on
+#
+#   NOTE: This application does not check for
+#         improper input.
+#
+############################################
+
 import sys
 import socket
 
+# Print out usage statement, if the user has provided commandline arguments
 if len(sys.argv) != 2:
     print 'usage:', sys.argv[0], '<port>'
     sys.exit(0)
 
+# Gather PORT number from commandline argument
 HOST = ''
 PORT = int(sys.argv[1])
+
+# Create a socket using IPv4, and bind and listen on given port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
+
+# Accept an incomming connection, then recv and print its message
 conn, addr = s.accept()
-
-print 'Connected by', addr
-
+print 'Connection by:', addr
 data = conn.recv(1024)
 print 'msg:      ', data
 
+# Get return message from the user and send it to the client
 data = raw_input('send back: ')
 conn.sendall(data)
 
+# Close the socket and connection
 conn.close()
 
 
